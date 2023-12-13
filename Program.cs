0882b571a8bd7885eed16c64de56c8b3e25c939b
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-12-13 11:13:27 -0700

    coding demostrating basic operations with directories

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace Directory_DirectoryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"c:\temp\folder1");
+
+            //var files = Directory.GetFiles(@"c:\projects\Directory_DirectoryInfo", "*.*", SearchOption.AllDirectories);
+            //foreach (var file in files)
+            //    Console.WriteLine(file);
+
+            var directories = Directory.GetDirectories(@"c:\projects\Directory_DirectoryInfo", "*.*", SearchOption.AllDirectories);
+            foreach (var directory in directories)
+                Console.WriteLine(directory);
+
+            Directory.Exists("...");
+
+            var directoryInfo = new DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+        }
+    }
+}
